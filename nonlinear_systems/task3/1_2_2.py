dc55982241ae82ae94ac2827802fedec6f39c073
Artem Kondratev <112778404+artem-kondratew@users.noreply.github.com>
2025-01-29 11:17:24 +0300

    add task3 part1

---
+++ nonlinear_systems/task3/1_2_2.py
@@ -0,0 +1,98 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+
+x, t = [2, 0.5], 0 #Начальные значения
+
+t1 = 30.0 #Период симуляции
+dt = 0.0001 #Шаг по времени
+
+solution = [] #Массивы для хранения значений
+tarr = []
+uarr = []
+z2list = []
+derx = []
+sigmaarr = []
+
+
+def rhs(t, state): # Функция возвращает правые части уравнений
+
+    x1, x2 = state
+
+    m = 4
+    k = 1.5 + 0.4 * np.sin(2 * t)
+
+    sigma = x2 + x1
+
+    
+    e = 0.001  
+    km = 1.9 
+    ro = km * x2 * x2
+    
+    if sigma > e:
+        sign = 1
+    elif np.abs(sigma) <= e:
+        sign = sigma / e
+    else:
+        sign = -1
+    
+    u = -ro * sign - m * x2 - np.sign(sigma)
+    
+    uarr.append(u)
+    sigmaarr.append(sigma)
+
+    dx1 = x2
+    dx2 = (u - k * x2 * abs(x2)) / m
+    
+    return [dx1, dx2]
+
+while t < t1:
+    dx = rhs(t, x)
+    x = [x[0]+dx[0]*dt, x[1]+dx[1]*dt] # Применяем метожд Эйлера
+
+    derx.append(dx)
+    solution.append(x)
+    tarr.append(t)
+    t += dt
+
+y1, y2 = zip(*solution)
+dy1, dy2 = zip(*derx)
+
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(7,5), sharex=True) # Вывод графиков (добавить вывод u)
+ax1.set_xlabel('Time, s')
+ax1.set_ylabel('X1')
+ax1.plot(tarr, y1, 'k-')
+ax1.grid(True)
+
+ax2.set_xlabel('Time, s')
+ax2.set_ylabel('X2')
+ax2.plot(tarr, y2, 'k-')
+ax2.grid(True)
+
+ax3.set_xlabel('Time, s')
+ax3.set_ylabel('X1, X2')
+ax3.plot(tarr, y2, 'b-')
+ax3.plot(tarr, y1, 'r-')
+ax3.grid(True)
+
+ax4.set_xlabel('Time, s')
+ax4.set_ylabel('sigma')
+ax4.plot(tarr, sigmaarr, 'b-')
+ax4.grid(True)
+
+fi2, (ax21, ax22, ax23) = plt.subplots(3, 1, figsize=(7,5), sharex=True)
+ax21.set_xlabel('Time, s')
+ax21.set_ylabel('dX1')
+ax21.plot(tarr, dy1, 'k-')
+ax21.grid(True)
+
+ax22.set_xlabel('Time, s')
+ax22.set_ylabel('dX2')
+ax22.plot(tarr, dy2, 'k-')
+ax22.grid(True)
+
+ax23.set_xlabel('Time, s')
+ax23.set_ylabel('u')
+ax23.plot(tarr, uarr, 'k-')
+ax23.grid(True)
+
+plt.show()
---
+++ nonlinear_systems/task3/1_5.py
@@ -0,0 +1,108 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+
+x, t = [2, 0.5, 0, 1], 0 #Начальные значения
+u = 0
+
+t1 = 30.0 #Период симуляции
+dt = 0.0001 #Шаг по времени
+
+solution = [] #Массивы для хранения значений
+tarr = []
+uarr = []
+derx = []
+
+
+def sign_exp(x, p):
+    return np.sign(x) * np.abs(x) ** p
+
+
+def observer(state):
+    x1, x2, sigma1e, sigma2e = state
+    sigma1 = x1 + x2
+
+    k1 = 15
+    k2 = 11
+
+    dsigma1e = -k1 * sign_exp(sigma1e - sigma1, 0.5) + sigma2e
+    dsigma2e = -k2 * np.sign(sigma1e - sigma1)
+
+    return [dsigma1e, dsigma2e]
+
+
+def rhs(t, state):
+    global u
+
+    x1, x2, _, sigma2e = state
+
+    m = 4
+    k = 1.5 + 0.4 * np.sin(2 * t)
+
+    sigma = x2 + x1
+    beta = 1
+    
+    s = sigma2e + beta * sign_exp(sigma, 0.5)
+    v = -10 * np.sign(s)
+    u += v * dt
+    
+    uarr.append(u)
+
+    dx1 = x2
+    dx2 = (u - k * x2 * np.abs(x2)) / m
+    
+    return [dx1, dx2]
+
+
+while t < t1:
+    dsigmae = observer(x)
+    dx = rhs(t, x)
+
+    x[2] = x[2] + dsigmae[0] * dt
+    x[3] = x[3] + dsigmae[1] * dt
+
+    x[0] = x[0] + dx[0] * dt
+    x[1] = x[1] + dx[1] * dt
+
+    derx.append(dx)
+    solution.append(x[:2])
+    tarr.append(t)
+    t += dt
+
+
+y1, y2 = zip(*solution)
+dy1, dy2 = zip(*derx)
+
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(7,5), sharex=True) # Вывод графиков (добавить вывод u)
+ax1.set_xlabel('Time, s')
+ax1.set_ylabel('X1')
+ax1.plot(tarr, y1, 'k-')
+ax1.grid(True)
+
+ax2.set_xlabel('Time, s')
+ax2.set_ylabel('X2')
+ax2.plot(tarr, y2, 'k-')
+ax2.grid(True)
+
+ax3.set_xlabel('Time, s')
+ax3.set_ylabel('X1, X2')
+ax3.plot(tarr, y2, 'b-')
+ax3.plot(tarr, y1, 'r-')
+ax3.grid(True)
+
+fi2, (ax21, ax22, ax23) = plt.subplots(3, 1, figsize=(7,5), sharex=True)
+ax21.set_xlabel('Time, s')
+ax21.set_ylabel('dX1')
+ax21.plot(tarr, dy1, 'k-')
+ax21.grid(True)
+
+ax22.set_xlabel('Time, s')
+ax22.set_ylabel('dX2')
+ax22.plot(tarr, dy2, 'k-')
+ax22.grid(True)
+
+ax23.set_xlabel('Time, s')
+ax23.set_ylabel('u')
+ax23.plot(tarr, uarr, 'k-')
+ax23.grid(True)
+
+plt.show()
